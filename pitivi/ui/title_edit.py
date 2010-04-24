572f2aef932d31501b6a62338c7511bbe4554f65
Dafydd Harries <daf@rhydd.org>
2010-04-24 18:16:16 -0400

    title editor: add toolbar; use it for font/color

--- pitivi/ui/title_edit.py
+++ pitivi/ui/title_edit.py
@@ -5,15 +5,15 @@ from pitivi.ui.glade import GladeWindow
 
 def get_color(c):
     return (
-        c.props.color.red_float,
-        c.props.color.green_float,
-        c.props.color.blue_float,
-        c.props.alpha / 65535.0)
+        c.props.current_color.red_float,
+        c.props.current_color.green_float,
+        c.props.current_color.blue_float,
+        c.props.current_alpha / 65535.0)
 
 def set_color(c, t):
-    c.props.color = gtk.gdk.Color(
+    c.props.current_color = gtk.gdk.Color(
         int(t[0] * 65535.0), int(t[1] * 65535.0), int(t[2] * 65535.0))
-    c.props.alpha = int(t[3] * 65535.0)
+    c.props.current_alpha = int(t[3] * 65535.0)
 
 alignments = [
         (0.0, 0.0), (0.5, 0.0), (1.0, 0.0),
@@ -35,9 +35,62 @@ class TitleEditDialog(GladeWindow):
         self.x_alignment = 0.5
         self.y_alignment = 0.5
 
+        self.widgets['color_button'].connect('clicked', self._run_color_dialog)
+        self.widgets['font_button'].connect('clicked', self._run_font_dialog)
+
         # Hack: GladeWindow hides TitleEditDialog's run() with gtk.Dialog's;
         # undo that.
         del self.run
+
+    def _run_color_dialog(self, _button):
+        dialog = gtk.Dialog()
+        content_area = dialog.get_content_area()
+
+        fg_frame = gtk.Frame("Foreground color")
+        fg_color_selection = gtk.ColorSelection()
+        fg_color_selection.props.has_opacity_control = True
+        set_color(fg_color_selection, self.fg_color)
+
+        bg_frame = gtk.Frame("Background color")
+        bg_color_selection = gtk.ColorSelection()
+        bg_color_selection.props.has_opacity_control = True
+        set_color(bg_color_selection, self.bg_color)
+
+        fg_frame.add(fg_color_selection)
+        bg_frame.add(bg_color_selection)
+        content_area.pack_start(fg_frame, True)
+        content_area.pack_start(bg_frame, True)
+        dialog.add_button(gtk.STOCK_APPLY, gtk.RESPONSE_OK)
+        dialog.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+
+        dialog.show_all()
+        response = dialog.run()
+        dialog.destroy()
+
+        if response == gtk.RESPONSE_OK:
+            self.fg_color = get_color(fg_color_selection)
+            self.bg_color = get_color(bg_color_selection)
+
+    def _run_font_dialog(self, _button):
+        dialog = gtk.Dialog()
+        content_area = dialog.get_content_area()
+
+        font_selection = gtk.FontSelection()
+        content_area.pack_start(font_selection, True)
+        font_selection.props.font_name = '%s %d' % (self.font, self.text_size)
+
+        dialog.add_button(gtk.STOCK_APPLY, gtk.RESPONSE_OK)
+        dialog.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+
+        dialog.show_all()
+        response = dialog.run()
+        dialog.destroy()
+
+        if response == gtk.RESPONSE_OK:
+            (self.font, size_str) = \
+                font_selection.props.font_name.rsplit(None, 1)
+            self.text_size = int(size_str)
+            print (self.font, self.text_size)
 
     def set(self, **kw):
         self.__dict__.update(kw)
@@ -45,12 +98,6 @@ class TitleEditDialog(GladeWindow):
     def _copy_to_dialog(self):
         buffer = self.widgets['textview'].props.buffer
         buffer.set_text(self.text)
-
-        set_color(self.widgets['bgcolor_button'], self.bg_color)
-        set_color(self.widgets['fgcolor_button'], self.fg_color)
-
-        self.widgets['fontbutton'].props.font_name = \
-            '%s %d' % (self.font, self.text_size)
 
         for i, (x_alignment, y_alignment) in enumerate(alignments):
             if (self.x_alignment == x_alignment and
@@ -60,13 +107,6 @@ class TitleEditDialog(GladeWindow):
     def _copy_from_dialog(self):
         buffer = self.widgets['textview'].props.buffer
         self.text = buffer.get_text(*buffer.get_bounds())
-
-        font_name = self.widgets['fontbutton'].props.font_name
-        self.font, size_str = font_name.rsplit(None, 1)
-        self.text_size = int(size_str)
-
-        self.bg_color = get_color(self.widgets['bgcolor_button'])
-        self.fg_color = get_color(self.widgets['fgcolor_button'])
 
         for i, (x_alignment, y_alignment) in enumerate(alignments):
             if self.widgets['radiobutton%d' % (i + 1)].props.active:
